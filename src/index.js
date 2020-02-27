be5efe41014e2c376c519c9444d5aa914a14058d
Lotus <38220115+LotusJS@users.noreply.github.com>
2020-02-28 01:18:11 +0300

    confirmed working with Slack with adjusted regex values

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 "use strict";
 
-//var discord = require("./services/discord");
-
 class chatlogLineParser {
-  //Constructor
-  constructor() {
-    this.username = null;
-    this.timeStamp = null;
-    this.currentMessage = null;
-    this.messages = [];
-  }
-
   //Begin the whole process of receiving the input and return a parsed format.
   static parse(logFile) {
     const parser = new chatlogLineParser();
@@ -59,9 +49,13 @@ module.exports = chatlogLineParser;
 //Try to match a Discord username+timestamp format. Example : LotusYesterday at 10:38 PM, LotusToday at 7:24 AM, LotusLast Friday at 10:26 AM, Lotus07/24/2019
 function sortUserTimeBool(line) {
   const ddmmyyReg = /((0?[13578]|10|12)(-|\/)(([1-9])|(0[1-9])|([12])([0-9]?)|(3[01]?))(-|\/)((19)([2-9])(\d{1})|(20)([01])(\d{1})|([8901])(\d{1}))|(0?[2469]|11)(-|\/)(([1-9])|(0[1-9])|([12])([0-9]?)|(3[0]?))(-|\/)((19)([2-9])(\d{1})|(20)([01])(\d{1})|([8901])(\d{1})))$/;
-  const daytimeReg = /(?=Last|Yesterday|Today)(.*)((([0]?[1-9]|1[0-2])(:|\.)[0-5][0-9]((:|\.)[0-5][0-9])?( )?(AM|am|aM|Am|PM|pm|pM|Pm))|(([0]?[0-9]|1[0-9]|2[0-3])(:|\.)[0-5][0-9]((:|\.)[0-5][0-9])?))$/;
+  const daytimeReg = /(?=Last|Yesterday|Today)(.*)|((([0]?[1-9]|1[0-2])(:|\.)[0-5][0-9]((:|\.)[0-5][0-9])?( )?(AM|am|aM|Am|PM|pm|pM|Pm))|(([0]?[0-9]|1[0-9]|2[0-3])(:|\.)[0-5][0-9]((:|\.)[0-5][0-9])?))$/;
 
-  let sorted = line.match(ddmmyyReg);
+  let sorted;
+  //service.forEach;
+  if (!sorted) {
+    line.match(ddmmyyReg);
+  }
   if (!sorted) {
     sorted = line.match(daytimeReg);
   }
