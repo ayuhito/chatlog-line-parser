22a0af74f2f96b21584fbe1424687ce189e58320
Lotus <38220115+LotusJS@users.noreply.github.com>
2020-03-02 13:15:55 +0300

    Added IRC support

--- index.js
+++ index.js
@@ -28,11 +28,18 @@ class chatlogLineParser {
     if (sortLine) {
       this.username = sortLine.username;
       this.timeStamp = sortLine.timeStamp;
+      this.irc = sortLine.irc;
     }
 
     //Identify if it is an actual message, then package it away.
-    if (!sortLine) {
-      this.currentMessage = line;
+    if (!sortLine || this.irc) {
+      this.currentMessage;
+      if (this.irc) {
+        this.currentMessage = line.substring(
+          line.indexOf("]") + this.username.length + 4,
+          line.length
+        );
+      }
       this.messages = {
         username: this.username,
         timeStamp: this.timeStamp,
@@ -50,22 +57,41 @@ module.exports = chatlogLineParser;
 function sortUserTimeBool(line) {
   const ddmmyyReg = /((0?[13578]|10|12)(-|\/)(([1-9])|(0[1-9])|([12])([0-9]?)|(3[01]?))(-|\/)((19)([2-9])(\d{1})|(20)([01])(\d{1})|([8901])(\d{1}))|(0?[2469]|11)(-|\/)(([1-9])|(0[1-9])|([12])([0-9]?)|(3[0]?))(-|\/)((19)([2-9])(\d{1})|(20)([01])(\d{1})|([8901])(\d{1})))$/;
   const daytimeReg = /(?=Last|Yesterday|Today)(.*)|((([0]?[1-9]|1[0-2])(:|\.)[0-5][0-9]((:|\.)[0-5][0-9])?( )?(AM|am|aM|Am|PM|pm|pM|Pm))|(([0]?[0-9]|1[0-9]|2[0-3])(:|\.)[0-5][0-9]((:|\.)[0-5][0-9])?))$/;
+  const IRCReg = /^([[0-9]{1,2}:[0-9][0-9].*?)\]/;
+  const telegramReg = null;
+
+  let irc = false;
 
   let sorted = line.match(ddmmyyReg);
   if (!sorted) {
     sorted = line.match(daytimeReg);
+  }
+  if (!sorted) {
+    irc = true;
+    sorted = line.match(IRCReg);
   }
 
   let username;
   if (sorted && sorted.length) {
     username = line.substring(0, line.length - sorted[0].length);
   }
+  if (irc) {
+    username = line.substring(sorted[0].length + 1, line.indexOf(": "));
+  }
 
+  sorted[0] = sorted[0].replace(/[\[\]]/g, "");
   //Ternary operator but Prettier doing weird stuff with formatting.
   return !sorted
     ? null
     : {
         username: String(username),
-        timeStamp: String(sorted[0])
+        timeStamp: String(sorted[0]),
+        irc: Boolean(irc)
       };
 }
+
+console.log(
+  chatlogLineParser.parse(
+    "9:16 PM] Lotus: for one line, grrr, I'll figure it out myself\n[9:17 PM] justis: <3 I'm kidding.\n[9:17 PM] justis: Knock yourself out.\n[9:17 PM] Lotus: I just looked at the regex and reallly, what other sort of regex am I gonna use lol"
+  )
+);
